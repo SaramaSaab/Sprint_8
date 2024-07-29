ec0214885eb2cc4eafec829e08419c9089b0a2fd
Viktoriya Gorlachuk <Sarama.Sab@yandex.ru>
2024-07-29 11:38:58 +0300

    add test project fpr 8 sprint

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,15 @@
+public class Praktikum {
+
+    public static void main(String[] args) {
+
+        Account account = new Account("Тимоти Шаламе ");
+        Boolean result = account.checkNameToEmboss();
+        System.out.println(result);
+        /*
+             В этом методе заложи логику работы с классом Account.
+             Нужно создать экземпляр класса Account: в качестве аргумента передать тестируемое имя
+             и вызвать метод, который проверяет, можно ли использовать фамилию и имя для печати на банковской карте.
+         */
+    }
+
+}
